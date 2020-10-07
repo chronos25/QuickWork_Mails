930287fb7262c2fb17cd2d2bd9b6718909551a2a
chronos25 <ritikrajsrivastava90@gmail.com>
2020-10-07 23:35:49 +0530

    add: feature mongoAtlas

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ const index = require('./index.js');
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://admin:<password>@cluster0.ved6n.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
   console.log("Connected to database successfully");
   client.close();
 });
